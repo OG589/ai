dd791d2794751df8c21a2184df0414a66c8a721b
OG589 <sohailk985012@gmail.com>
2025-04-28 00:40:04 +0530

    Update bfsdfs.py

--- bfsdfs.py
+++ bfsdfs.py
@@ -3,41 +3,49 @@ import collections
 def bfs(graph, root):
     visited = set()
     queue = collections.deque([root])
+    traversal_order = []
     
     while queue:
         vertex = queue.popleft()
         if vertex not in visited:
             visited.add(vertex)
-            for i in graph[vertex]:
-                if i not in visited:
-                    queue.append(i)
+            traversal_order.append(vertex)
+            for neighbor in graph[vertex]:
+                if neighbor not in visited:
+                    queue.append(neighbor)
                     
-    return visited
+    return traversal_order
 
 def dfs(graph, root):
     visited = set()
-    stack = [root] 
+    stack = [root]
+    traversal_order = []
 
     while stack:
         vertex = stack.pop()
         if vertex not in visited:
             visited.add(vertex)
-            for neighbor in graph[vertex]:
+            traversal_order.append(vertex)
+            for neighbor in reversed(graph[vertex]):
                 if neighbor not in visited:
                     stack.append(neighbor)
     
-    return visited
+    return traversal_order
 
-if _name_ == "_main_":
-    graph = {
-    'A': ['B', 'C'],
-    'B': ['D', 'E'],
-    'C': ['F'],
-    'D': [],
-    'E': ['F'],
-    'F': []
-}
+if __name__ == "__main__":
+    graph = {}
+    num_vertices = int(input("Enter number of vertices: "))
 
-    
-    print("BFS:", bfs(graph, 'A'))
-    print("DFS:", dfs(graph, 'A'))
+    for _ in range(num_vertices):
+        vertex = input("\nEnter vertex name: ")
+        neighbors = input(f"Enter neighbors of {vertex} separated by space (if none, press Enter): ").split()
+        graph[vertex] = neighbors
+
+    root = input("\nEnter starting vertex: ")
+
+    bfs_order = bfs(graph, root)
+    dfs_order = dfs(graph, root)
+
+    print("\nBFS Traversal:", " -> ".join(bfs_order))
+    print("DFS Traversal:", " -> ".join(dfs_order))
+
